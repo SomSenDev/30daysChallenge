0f5ff15397873c185b10875489cb842a41db6270
Somalia Senior Developers <140841348+SomSenDev@users.noreply.github.com>
2023-08-15 01:02:28 +0300

    Add files via upload

---
+++ Day14 Currency Converter/script.js
@@ -0,0 +1,257 @@
+let country_list = {
+    "AED" : "AE",
+    "AFN" : "AF",
+    "XCD" : "AG",
+    "ALL" : "AL",
+    "AMD" : "AM",
+    "ANG" : "AN",
+    "AOA" : "AO",
+    "AQD" : "AQ",
+    "ARS" : "AR",
+    "AUD" : "AU",
+    "AZN" : "AZ",
+    "BAM" : "BA",
+    "BBD" : "BB",
+    "BDT" : "BD",
+    "XOF" : "BE",
+    "BGN" : "BG",
+    "BHD" : "BH",
+    "BIF" : "BI",
+    "BMD" : "BM",
+    "BND" : "BN",
+    "BOB" : "BO",
+    "BRL" : "BR",
+    "BSD" : "BS",
+    "NOK" : "BV",
+    "BWP" : "BW",
+    "BYR" : "BY",
+    "BZD" : "BZ",
+    "CAD" : "CA",
+    "CDF" : "CD",
+    "XAF" : "CF",
+    "CHF" : "CH",
+    "CLP" : "CL",
+    "CNY" : "CN",
+    "COP" : "CO",
+    "CRC" : "CR",
+    "CUP" : "CU",
+    "CVE" : "CV",
+    "CYP" : "CY",
+    "CZK" : "CZ",
+    "DJF" : "DJ",
+    "DKK" : "DK",
+    "DOP" : "DO",
+    "DZD" : "DZ",
+    "ECS" : "EC",
+    "EEK" : "EE",
+    "EGP" : "EG",
+    "ETB" : "ET",
+    "EUR" : "FR",
+    "FJD" : "FJ",
+    "FKP" : "FK",
+    "GBP" : "GB",
+    "GEL" : "GE",
+    "GGP" : "GG",
+    "GHS" : "GH",
+    "GIP" : "GI",
+    "GMD" : "GM",
+    "GNF" : "GN",
+    "GTQ" : "GT",
+    "GYD" : "GY",
+    "HKD" : "HK",
+    "HNL" : "HN",
+    "HRK" : "HR",
+    "HTG" : "HT",
+    "HUF" : "HU",
+    "IDR" : "ID",
+    "ILS" : "IL",
+    "INR" : "IN",
+    "IQD" : "IQ",
+    "IRR" : "IR",
+    "ISK" : "IS",
+    "JMD" : "JM",
+    "JOD" : "JO",
+    "JPY" : "JP",
+    "KES" : "KE",
+    "KGS" : "KG",
+    "KHR" : "KH",
+    "KMF" : "KM",
+    "KPW" : "KP",
+    "KRW" : "KR",
+    "KWD" : "KW",
+    "KYD" : "KY",
+    "KZT" : "KZ",
+    "LAK" : "LA",
+    "LBP" : "LB",
+    "LKR" : "LK",
+    "LRD" : "LR",
+    "LSL" : "LS",
+    "LTL" : "LT",
+    "LVL" : "LV",
+    "LYD" : "LY",
+    "MAD" : "MA",
+    "MDL" : "MD",
+    "MGA" : "MG",
+    "MKD" : "MK",
+    "MMK" : "MM",
+    "MNT" : "MN",
+    "MOP" : "MO",
+    "MRO" : "MR",
+    "MTL" : "MT",
+    "MUR" : "MU",
+    "MVR" : "MV",
+    "MWK" : "MW",
+    "MXN" : "MX",
+    "MYR" : "MY",
+    "MZN" : "MZ",
+    "NAD" : "NA",
+    "XPF" : "NC",
+    "NGN" : "NG",
+    "NIO" : "NI",
+    "NPR" : "NP",
+    "NZD" : "NZ",
+    "OMR" : "OM",
+    "PAB" : "PA",
+    "PEN" : "PE",
+    "PGK" : "PG",
+    "PHP" : "PH",
+    "PKR" : "PK",
+    "PLN" : "PL",
+    "PYG" : "PY",
+    "QAR" : "QA",
+    "RON" : "RO",
+    "RSD" : "RS",
+    "RUB" : "RU",
+    "RWF" : "RW",
+    "SAR" : "SA",
+    "SBD" : "SB",
+    "SCR" : "SC",
+    "SDG" : "SD",
+    "SEK" : "SE",
+    "SGD" : "SG",
+    "SKK" : "SK",
+    "SLL" : "SL",
+    "SOS" : "SO",
+    "SRD" : "SR",
+    "STD" : "ST",
+    "SVC" : "SV",
+    "SYP" : "SY",
+    "SZL" : "SZ",
+    "THB" : "TH",
+    "TJS" : "TJ",
+    "TMT" : "TM",
+    "TND" : "TN",
+    "TOP" : "TO",
+    "TRY" : "TR",
+    "TTD" : "TT",
+    "TWD" : "TW",
+    "TZS" : "TZ",
+    "UAH" : "UA",
+    "UGX" : "UG",
+    "USD" : "US",
+    "UYU" : "UY",
+    "UZS" : "UZ",
+    "VEF" : "VE",
+    "VND" : "VN",
+    "VUV" : "VU",
+    "YER" : "YE",
+    "ZAR" : "ZA",
+    "ZMK" : "ZM",
+    "ZWD" : "ZW"
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const input = document.querySelector('.currencyInput');
+    const selectFrom = document.querySelector('select.from');
+    const selectTo = document.querySelector('select.to');
+    const convert = document.querySelector('.convert');
+    const result = document.querySelector('.result');
+    const imgFrom = document.querySelector('.imgFrom');
+    const imgTo = document.querySelector('.imgTo');
+    const switchBtn = document.querySelector('.switch');
+  
+
+    const api = '6ddac1f99c3c72c25e762b6b6f6f046b';
+
+    //even listener the options
+    selectFrom.addEventListener('change', changeImg);
+    selectTo.addEventListener('change', changeImg);
+
+    
+
+
+
+
+    //function to populate the options to the country_list
+  function populateSelect() {
+  for (var currency in country_list) {
+    const option = document.createElement("option");
+    option.value = currency;
+    option.text = currency ;
+    selectFrom.appendChild(option);
+    selectTo.appendChild(option.cloneNode(true));
+  }
+  changeImg();
+}
+
+//function to change the img to the selected currency
+
+
+function changeImg() {
+  const from = selectFrom.value;
+  const to = selectTo.value;
+  for (let currency in country_list) {
+    if (currency === from) {
+      let lower = country_list[currency].toLowerCase();
+      imgFrom.src = `https://flagcdn.com/h24/${lower}.png`;
+    }if (currency === to) {
+      let lower = country_list[currency].toLowerCase();
+      imgTo.src = `https://flagcdn.com/h24/${lower}.png`;
+    }
+  }
+}
+
+//function to convert the currency
+convert.addEventListener('click', function() {
+
+    //get the values from the input and the select
+    const amount = input.value;
+    const from = selectFrom.value;
+    const to = selectTo.value;
+    
+    //fetch the api
+    fetch(`http://apilayer.net/api/live?access_key=${api}&currencies=${from},${to}&format=1`)
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data);
+        const rate = data.quotes;
+        const rateFrom = rate[`USD${from}`];
+        const rateTo = rate[`USD${to}`];
+        const resultValue = (amount * rateTo) / rateFrom;
+        result.innerHTML = `${amount} ${from} =
+        ${resultValue.toFixed(2)} ${to}`;
+       
+    }
+    )
+    .catch(err => console.log(err));
+
+});
+
+//function to switch the values of the select
+switchBtn.addEventListener('click', function() {
+
+    const from = selectFrom.value;
+    const to = selectTo.value;
+    selectFrom.value = to;
+    selectTo.value = from;
+    changeImg();
+});
+
+
+
+
+
+populateSelect();  
+
+
+});
